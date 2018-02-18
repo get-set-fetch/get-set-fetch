9fda09106ee69c6fa750696c7f7f7cd2ca1cf1fe
Andrei Sabau <andrei@asabau.com>
2018-02-18 20:09:19 +0200

    sql storage tests, #3

---
+++ test/storage/test-resource-crud.js
@@ -0,0 +1,71 @@
+require('chai/register-assert');
+const connections = require('./connections.json');
+
+const Storage = require.main.require('lib/storage/Storage');
+
+connections.forEach((conn) => {
+  describe(`Test Storage Resource - CRUD, using connection ${conn.info}`, () => {
+    let Site = null;
+    let Resource = null;
+    const expectedResource = { id: null, siteId: null, url: 'http://siteA/resourceA' };
+
+    before(async () => {
+      ({ Site, Resource } = await Storage.init(conn));
+      await Site.delAll();
+      const site = new Site('siteA', 'http://siteA');
+      await site.save();
+      expectedResource.siteId = site.id;
+    });
+
+    beforeEach(async () => {
+      // cleanup
+      await Resource.delAll();
+
+      // save resource
+      const resource = new Resource(expectedResource.siteId, expectedResource.url);
+      await resource.save();
+      assert.isNotNull(resource.id);
+      expectedResource.id = resource.id;
+    });
+
+    after(() => {
+      Storage.close();
+    });
+
+    it('get', async () => {
+      // get resource by id
+      const resourceById = await Resource.get(expectedResource.id);
+      assert.instanceOf(resourceById, Resource);
+      assert.strictEqual(String(expectedResource.siteId), String(resourceById.siteId));
+      assert.strictEqual(expectedResource.url, resourceById.url);
+
+      // get resource by url
+      const resourceByUrl = await Resource.get(expectedResource.url);
+      assert.instanceOf(resourceByUrl, Resource);
+      assert.strictEqual(String(expectedResource.id), String(resourceByUrl.id));
+      assert.strictEqual(String(expectedResource.siteId), String(resourceByUrl.siteId));
+    });
+
+    it('update', async () => {
+      // update resource
+      const updateResource = await Resource.get(expectedResource.id);
+      updateResource.url = 'http://siteA/resourceA_updated';
+      await updateResource.update();
+
+      // get and compare
+      const getResource = await Resource.get(expectedResource.id);
+      assert.strictEqual(String(expectedResource.siteId), String(getResource.siteId));
+      assert.strictEqual(updateResource.url, getResource.url);
+    });
+
+    it('delete', async () => {
+      // delete site
+      const delResource = await Resource.get(expectedResource.id);
+      await delResource.del();
+
+      // get and compare
+      const getResource = await Resource.get(expectedResource.id);
+      assert.isNull(getResource);
+    });
+  });
+});
---
+++ test/storage/test-site-crud.js
@@ -0,0 +1,73 @@
+require('chai/register-assert');
+const connections = require('./connections.json');
+
+const Storage = require.main.require('lib/storage/Storage');
+
+connections.forEach((conn) => {
+  describe(`Test Storage Site - CRUD, using connection ${conn.info}`, () => {
+    let Site = null;
+    const expectedSite = {
+      id: null, name: 'siteA', url: 'http://siteA', requestHeaders: { 'User-Agent': 'custom-user-agent' },
+    };
+
+    before(async () => {
+      ({ Site } = await Storage.init(conn));
+    });
+
+    beforeEach(async () => {
+      // cleanup
+      await Site.delAll();
+
+      // save site
+      const site = new Site(expectedSite.name, expectedSite.url, expectedSite.requestHeaders);
+      await site.save();
+      assert.isNotNull(site.id);
+      expectedSite.id = site.id;
+    });
+
+    after(() => {
+      Storage.close();
+    });
+
+    it('get', async () => {
+      // get site by id
+      const siteById = await Site.get(expectedSite.id);
+      assert.instanceOf(siteById, Site);
+      assert.strictEqual(expectedSite.name, siteById.name);
+      assert.strictEqual(expectedSite.url, siteById.url);
+      assert.deepEqual(expectedSite.requestHeaders, siteById.requestHeaders);
+
+      // get site by name
+      const siteByName = await Site.get(expectedSite.name);
+      assert.instanceOf(siteByName, Site);
+      assert.strictEqual(String(expectedSite.id), String(siteByName.id));
+      assert.strictEqual(expectedSite.url, siteByName.url);
+      assert.deepEqual(expectedSite.requestHeaders, siteByName.requestHeaders);
+    });
+
+    it('update', async () => {
+      // update site
+      const updateSite = await Site.get(expectedSite.id);
+      updateSite.name = 'siteA_updated';
+      updateSite.url = 'http://siteA/updated';
+      updateSite.requestHeaders = { 'User-Agent': 'custom-user-agent-update' };
+      await updateSite.update();
+
+      // get and compare
+      const getSite = await Site.get(expectedSite.id);
+      assert.strictEqual(updateSite.name, getSite.name);
+      assert.strictEqual(updateSite.url, getSite.url);
+      assert.deepEqual(updateSite.requestHeaders, getSite.requestHeaders);
+    });
+
+    it('delete', async () => {
+      // delete site
+      const delSite = await Site.get(expectedSite.id);
+      await delSite.del();
+
+      // get and compare
+      const getSite = await Site.get(expectedSite.id);
+      assert.isNull(getSite);
+    });
+  });
+});
