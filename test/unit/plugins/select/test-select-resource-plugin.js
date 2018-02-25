db8ae7f9c84cc4f4fe70e49d4255e4469e9d94a2
Andrei Sabau <andrei@asabau.com>
2018-02-25 12:53:22 +0200

    select resource to crawl #18

---
+++ test/unit/plugins/select/test-select-resource-plugin.js
@@ -0,0 +1,25 @@
+// require('chai/register-assert');
+const chai = require('chai');
+const spies = require('chai-spies');
+
+chai.use(spies);
+
+const SelectResourcePlugin = require.main.require('lib/plugins/select/SelectResourcePlugin');
+
+describe('Test NodeFetchPlugin', () => {
+  let selectResourcePlugin = null;
+
+  beforeEach(() => {
+    selectResourcePlugin = new SelectResourcePlugin();
+  });
+
+  it('apply', async () => {
+    const site = {
+      getResourceToCrawl: () => {},
+    };
+
+    const getResourceToCrawlSpy = chai.spy.on(site, 'getResourceToCrawl');
+    selectResourcePlugin.apply(site);
+    chai.expect(getResourceToCrawlSpy).to.have.been.called();
+  });
+});
