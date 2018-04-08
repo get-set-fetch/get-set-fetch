1218b74a03593ed70018a088c2eedbae71852b43
Andrei Sabau <andrei@asabau.com>
2018-04-08 18:40:48 +0300

    NodeFetchPlugin performs utf8 encoding only for html|text content type

--- test/unit/plugins/fetch/test-node-fetch-plugin.js
+++ test/unit/plugins/fetch/test-node-fetch-plugin.js
@@ -11,19 +11,19 @@ describe('Test NodeFetchPlugin', () => {
   });
 
   it('http fetch default port', async () => {
-    nock('http://www.site.com').get('/pageA').reply(200, 'httpA');
+    nock('http://www.site.com').get('/pageA').reply(200, 'httpA', { 'Content-Type': 'text/html; charset=utf-8' });
     const { content } = await nodeFetchPlugin.fetch({ url: 'http://www.site.com/pageA' });
     assert.strictEqual('httpA', content);
   });
 
   it('http fetch custom port', async () => {
-    nock('http://www.site.com:8090').get('/pageA').reply(200, 'httpA-8090');
+    nock('http://www.site.com:8090').get('/pageA').reply(200, 'httpA-8090', { 'Content-Type': 'text/html; charset=utf-8' });
     const { content } = await nodeFetchPlugin.fetch({ url: 'http://www.site.com:8090/pageA' });
     assert.strictEqual('httpA-8090', content);
   });
 
   it('https fetch default port', async () => {
-    nock('https://www.site.com').get('/pageA').reply(200, 'httspA');
+    nock('https://www.site.com').get('/pageA').reply(200, 'httspA', { 'Content-Type': 'text/html; charset=utf-8' });
     const { content } = await nodeFetchPlugin.fetch({ url: 'https://www.site.com/pageA' });
     assert.strictEqual('httspA', content);
   });
@@ -32,7 +32,7 @@ describe('Test NodeFetchPlugin', () => {
     const userAgent = 'custom-user-agent';
     nock('http://www.site.com')
       .matchHeader('User-Agent', userAgent)
-      .get('/pageA').reply(200, 'httpA');
+      .get('/pageA').reply(200, 'httpA', { 'Content-Type': 'text/html; charset=utf-8' });
 
     nodeFetchPlugin.opts = {
       reqHeaders: { 'User-Agent': userAgent },
