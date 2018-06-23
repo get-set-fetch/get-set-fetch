8fc973444872a886d4aade0f59f400b08671c044
Andrei Sabau <andrei@asabau.com>
2018-06-23 15:45:38 +0300

    test utils are now included in the exported files, closes #35

--- test/utils/TestUtils.js
+++ test/utils/TestUtils.js
@@ -74,8 +74,4 @@ class TestUtils {
   }
 }
 
-// add a require wrapper for get-set-fetch module
-// eslint-disable-next-line import/no-dynamic-require
-global.gsfRequire = name => require(path.join(__dirname, '..', '..', name));
-
 module.exports = TestUtils;
